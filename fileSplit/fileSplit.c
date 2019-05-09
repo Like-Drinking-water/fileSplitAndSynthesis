871eb3c039362590d4b31a00af04ada2e8718895
LikeDrinkingWater_glb <LikeDrinkingWater_glb@chl.com>
2019-05-09 15:18:44 +0800

    C

---
+++ fileSplit/fileSplit.c
@@ -0,0 +1,206 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<process.h>
+#include<direct.h>
+
+#define FILE_PATH_LENGTH 200
+#define SPLIT_FILE_SIZE 103809024
+
+/**
+* 打印作者信息 
+*/
+void author() {
+	printf("-------------------------------\n作者：chen\n联系邮箱：879070711@qq.com\n-------------------------------\n");
+}
+
+/**
+* 拷贝文件
+* @Param source[] 要被拷贝的字符串
+* @Param another[] 获取拷贝内容的字符串
+* @Param size 要拷贝的数量
+*/
+void copyString(char source[], char another[], int size) {
+	int j;
+	for (j = 0; j <= size; j++) {
+		another[j] = source[j];
+	}
+	another[j] = '\0';
+}
+
+/**
+* 将源文件的名字保存到特定文件中 
+* @Param filePath[] 拆分的文件的路径
+* @Param fileSplitDir[] 拆分文件存放的路径
+* @Param pathLastIndex 用来标志基础路径的最后下标 
+*/
+void sourceFileName(char filePath[], char fileSplitDir[], int pathLastIndex) {
+	FILE *theName;
+	int i = 0;
+	int j = 0;
+	int last;
+	char splitFilePath[FILE_PATH_LENGTH];
+	copyString(fileSplitDir, splitFilePath, pathLastIndex + 4);
+	//找到字符串结尾
+	while (filePath[i] != '\0') {
+		i++;
+	}
+	last = i;
+	//找到最后一个路径分隔符的下标
+	while (filePath[i] != '\\' && filePath[i] != '/') {
+		i--;
+	}
+	last -= i;
+	//给存放文件名称的文件命名 
+	splitFilePath[pathLastIndex + 5] = '/';
+	splitFilePath[pathLastIndex + 6] = 'n';
+	splitFilePath[pathLastIndex + 7] = 'a';
+	splitFilePath[pathLastIndex + 8] = 'm';
+	splitFilePath[pathLastIndex + 9] = 'e';
+	splitFilePath[pathLastIndex + 10] = '.';
+	splitFilePath[pathLastIndex + 11] = 'c';
+	splitFilePath[pathLastIndex + 12] = 'h';
+	splitFilePath[pathLastIndex + 13] = 'l';
+	splitFilePath[pathLastIndex + 14] = '\0';
+	theName = fopen(splitFilePath, "wb+");
+	if (theName == NULL) {
+		printf("Can not open the file!\n");
+		system("pause");
+		return;
+	}
+	//将文件名保存起来 
+	while (filePath[i + j + 1] != '\0') {
+		fputc(filePath[i + j + 1], theName);
+		j++;
+	}
+	//操作结束后关闭文件 
+	fclose(theName);
+}
+
+/**
+* 修改拆分文件的名称
+* @Param splitFilePath[] 最初的拆分文件名称
+* @Param start 标志名称开始的下标
+* @Param i 标志已经使用的文件名 
+*/
+void splitFileName(char splitFilePath[], int start, int i) {
+	int j;
+	for (j = 1; j <= 5; j++) {
+		splitFilePath[start + j] = (i % 10) + '0';
+		i /= 10;
+	}
+}
+
+
+
+/**
+* 创建存放拆分文件的文件夹
+* @Param fileBasePath[] 文件的基础路径
+* @Param fileSplitDir[] 拆分文件保存的文件夹
+* @Param size 文件的基础路径的大小
+* @Return 0 表示成功
+*/
+int createSplitDir(char fileBasePath[], char fileSplitDir[], int size) {
+	copyString(fileBasePath, fileSplitDir, size);
+	fileSplitDir[size + 1] = 's';
+	fileSplitDir[size + 2] = 'p';
+	fileSplitDir[size + 3] = 'l';
+	fileSplitDir[size + 4] = 'i';
+	fileSplitDir[size + 5] = 't';
+	fileSplitDir[size + 6] = '\0';
+	return _mkdir(fileSplitDir);
+}
+
+/**
+* 获取文件的基础路径
+* @Param filePath[] 文件的路径字符串
+* @Param fileBasePath[] 文件的基础路径字符串
+* @Return 返回基础路径字符串结尾标志的下标
+*/
+int getBasePath(char filePath[], char fileBasePath[]) {
+	int i = 0;
+	//找到字符串结尾
+	while (filePath[i] != '\0') {
+		i++;
+	}
+	//找到最后一个路径分隔符的下标
+	while (filePath[i] != '\\' && filePath[i] != '/') {
+		i--;
+	}
+	//将最后一个路径分隔符前的内容赋值给基础路径
+	copyString(filePath, fileBasePath, i);
+	
+	return (i + 1);
+}
+
+int main(void) {
+	//用来临时存放数据
+	char tmp;
+	int status, i, j, pathLastIndex;
+	FILE *fileBase, *fileSplit;
+	//用来保存文件的路径和文件的基础路径
+	char filePath[FILE_PATH_LENGTH], fileBasePath[FILE_PATH_LENGTH], fileSplitDir[FILE_PATH_LENGTH], splitFilePath[FILE_PATH_LENGTH];
+	author(); 
+	printf("请输入要拆分的文件的地址:");
+	scanf("%s",filePath);
+	printf("正在拆分请勿关闭......");
+	
+	//获取文件的基础路径并记录下字符串的最后下标
+	pathLastIndex = getBasePath(filePath, fileBasePath);
+	
+	fileBase = fopen(filePath, "rb");  
+	//判断是否成功打开文件
+	if (fileBase == NULL) {
+		printf("Can not open the file!\n");
+		system("pause");
+		return 0;
+	}
+	
+	//在要拆分的文件的路径下创建一个名为 split 的文件夹
+	status = createSplitDir(fileBasePath, fileSplitDir, pathLastIndex - 1);
+	//无法创建文件夹
+	if (status) {
+		printf("Can not create dir\n");
+		system("pause");
+		return 0;
+	}
+	sourceFileName(filePath, fileSplitDir, pathLastIndex);
+	//拷贝文件夹的路径
+	copyString(fileSplitDir, splitFilePath, pathLastIndex + 4);
+	splitFilePath[pathLastIndex + 5] = '/';
+	splitFilePath[pathLastIndex + 6] = '0';
+	splitFilePath[pathLastIndex + 7] = '0';
+	splitFilePath[pathLastIndex + 8] = '0';
+	splitFilePath[pathLastIndex + 9] = '0';
+	splitFilePath[pathLastIndex + 10] = '0';
+	splitFilePath[pathLastIndex + 11] = '.';
+	splitFilePath[pathLastIndex + 12] = 'c';
+	splitFilePath[pathLastIndex + 13] = 'h';
+	splitFilePath[pathLastIndex + 14] = 'l';
+	splitFilePath[pathLastIndex + 15] = '\0';
+	//读取文件
+	j = 0;
+	i = 0;
+	while (!(feof(fileBase))) {
+		fileSplit = fopen(splitFilePath, "wb+");
+		//判断是否成功打开文件
+		if (fileSplit == NULL) {
+			printf("Can not open the file!\n");
+			system("pause");
+			return 0;
+		}
+		//从文件中读取 99M 到拆分文件 当读取到 99M 或文件读取完成停止操作 
+		while (j < SPLIT_FILE_SIZE && !(feof(fileBase))) {
+			tmp = fgetc(fileBase);
+			fputc(tmp, fileSplit);
+			j++;
+		}
+		j = 0;
+		//关闭一个拆分文件 
+		fclose(fileSplit);
+		i++;
+		splitFileName(splitFilePath, pathLastIndex + 5, i);
+	}
+	fclose(fileBase);
+	
+	return 0;
+}
---
+++ fileSynthesis/fileSynthesis.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* 打印作者信息 
+*/
+void author() {
+	printf("-------------------------------\n作者：chen\n联系邮箱：879070711@qq.com\n-------------------------------\n");
+}
+
+/**
+* 用来获取拆分文件的名称 
+* @SplitFile 初始拆分文件的名称
+* @i 标志以获取的名称 
+*/ 
+void splitFileName(char splitFile[], int i) {
+	int j;
+	for (j = 0; j < 5; j++) {
+		splitFile[j] = (i % 10) + '0';
+		i /= 10;
+	}
+}
+
+int main(void) {
+	FILE *name, *result, *split;
+	char tmp;
+	int i = 0;
+	char resultFile[20], splitFile[10];
+	author();
+	printf("正在合成请勿关闭......");
+	//创建最终合成的文件
+	name = fopen("name.chl", "rb");
+	if (name == NULL) {
+		printf("Can not found name.chl!");
+		system("pause"); 
+	}
+	//获取文件名 
+	while (!(feof(name)))
+	{	
+		tmp = fgetc(name);
+		if (tmp != EOF) {
+			resultFile[i] = tmp;
+			i++;	
+		}
+	}
+	resultFile[i] = '\0';
+	fclose(name);
+	//读取完成后关闭并删除文件
+	remove("name.chl");
+	result = fopen(resultFile, "wb+");
+	//初始化拆分文件名称 
+	splitFile[0] = '0';
+	splitFile[1] = '0';
+	splitFile[2] = '0';
+	splitFile[3] = '0';
+	splitFile[4] = '0';
+	splitFile[5] = '.';
+	splitFile[6] = 'c';
+	splitFile[7] = 'h';
+	splitFile[8] = 'l';
+	splitFile[9] = '\0';
+	for (i = 0;;) {
+		split = fopen(splitFile, "rb");
+		//但没有拆分文件可以读取时退出 
+		if (split == NULL) {
+			break;
+		}
+		while (!(feof(split)))
+		{
+			tmp = fgetc(split);
+			fputc(tmp, result);	
+		}
+		//读取完成后关闭并删除文件 
+		fclose(split);
+		remove(splitFile);
+		i++;
+		splitFileName(splitFile, i);
+	}
+	fclose(result);
+	return 0;
+	
+}
